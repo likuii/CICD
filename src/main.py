d7e3d63ad6aa1bf8bf3eabca57a80b3ac1fef46f
ilikui <2563043733@qq.com>
2023-05-20 10:36:26 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
-import click
-from src.logic import area, area_zone
+from flask import Flask
 
-@click.command()
-@click.option(
-    "--location",
-    help="This specifies the location you want to know the time. For example, Lagos or London",
-)
-@click.option(
-    "--zone",
-    help="The timezone information you need. Ensure it is properly capitalized, for example CET or WAT",
-)
-def main(location, zone):
-    if location:
-        area(location)
-    if zone:
-        area_zone(zone)
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
 
 if __name__ == "__main__":
-    main()
+    app.run()
