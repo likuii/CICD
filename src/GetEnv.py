9174189d78c8965eee54dc1ac0d73e39949d9b4a
ilikui <2563043733@qq.com>
2023-05-21 11:14:55 +0800

    update

--- src/GetEnv.py
+++ src/GetEnv.py
@@ -6,7 +6,4 @@ import os
 if(os.environ["USER"]):
     user = os.environ["USER"]
     print(user)
-if(os.environ["PASSWORD"]):
-    password = os.environ["PASSWORD"]
-if(os.environ["HOST"]):
-    host = os.environ["HOST"]
+
